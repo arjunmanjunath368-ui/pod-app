d56297bbd4f1051601652d7fa5e76f007c5bb2fb
Arjun <arjun.manjunath368@gmail.com>
2026-06-03 01:03:30 -0500

    Personal goals + feel-good preset

--- components/MyGoals.tsx
+++ components/MyGoals.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { createClient } from "@/lib/supabase/client";
 
 const PRESETS = [
+  "Honestly, it's the feel good factor",
   "Lose weight",
   "Build muscle",
   "More stamina",
